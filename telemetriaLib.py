e3966b11d61523867f294047af9cb1c2b06be8d6
Victor Uceda <victoruceda@MacBook-Air-de-Victor.local>
2016-02-09 23:58:26 +0100

    pyMetria v1.0
    Aplicacion para recibir telemetría por PuertoSerie/Bluetooth y graficarla en tiempo real
    
    Protocolo de mensajes (en texto):
        "pT timestamp/señal11 señal12 ... señal1N|señal21 señal22 ... señal2M| ... | ... |señalK1 señalK2 ... señalKL"
            -timestamp: tiempo de mensaje (comun a todas las señale). Se pinta en el ejeX
            -N,M,K,J numeros diferenes
            -La señales en un mismo bloque (bloques separados por el simbolo \'|\') se pintan como diferentes lineas dentro de la misma grafica
    
    La aplicacion que envia los datos de telemetria elige que datos quiere graficar (todo es variable) aunque si que se necesita en esta version que e mantenga constante el numero de señales a lo largo de la ejecucion
    
    TODO (para versiones futuras):
        -Guardar los valores registrados a fichero
        -Permitir variar el numero de bloques/lineas en tiempo de ejecución
        -Establecer una comunicación bidireccional para hacer peticiones de información de titulos y etiquetas de las diferentes
    Aplicacio

--- telemetriaLib.py
+++ telemetriaLib.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
+'''
+pyMetria v1.0
+
+Aplicacion para recibir telemetría por PuertoSerie/Bluetooth y graficarla en tiempo real
+
+Protocolo de mensajes (en texto):
+    "pT timestamp/señal11 señal12 ... señal1N|señal21 señal22 ... señal2M| ... | ... |señalK1 señalK2 ... señalKL"
+        -timestamp: tiempo de mensaje (comun a todas las señale). Se pinta en el ejeX
+        -N,M,K,J numeros diferenes
+        -La señales en un mismo bloque (bloques separados por el simbolo '|') se pintan como diferentes lineas dentro de la misma grafica
+
+La aplicacion que envia los datos de telemetria elige que datos quiere graficar (todo es variable) aunque si que se necesita en esta version que e mantenga constante el numero de señales a lo largo de la ejecucion
+
+
+TODO (para versiones futuras):
+    -Guardar los valores registrados a fichero
+    -Permitir variar el numero de bloques/lineas en tiempo de ejecución
+    -Establecer una comunicación bidireccional para hacer peticiones de información de titulos y etiquetas de las diferentes señales
+
+'''
+
+
+
 import serial
+import time
 import matplotlib
 matplotlib.use('TKAgg')
 import os
@@ -7,24 +31,27 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import threading
 
+ANCHO_X_SEG = 60
+
 class Telemetria:
     '''
     Clase para guardar los datos de la telemetria y leerlos del serial/bluetooth
     '''
-    # pylint: disable=too-many-instance-attributes
-    # Eight is reasonable in this case.
     def __init__(self, serial_port):
-        self.connected = False
+        self.connected = True
         self.sp = serial.Serial(serial_port, 19200) #'/dev/cu.bqZUM_BT328-SPPDev'
         self.timeList = []
         self.data = []
         self.fig = plt.figure()
+        self.fig.canvas.set_window_title('pyTelemetría')
         self.ax = []
         self.lines = []
-        self.thread = threading.Thread(target=self.updateData, args=(self,))
 
     def getLines(self):
         return self.lines
+
+    def getAx(self, i):
+        return self.ax[i]
 
     def getTimeList(self):
         return self.timeList
@@ -36,43 +63,49 @@ class Telemetria:
         return self.fig
 
     def inicializar(self):
+        '''
+        Metodo para inicializar la lectura de datos de telemetria por BT. Lee un mensaje y lo decodifica determinando en funcion de este mensaje el numero de gráficas y lineas por grafica necesarias.
+        Ejecutar siempre al comienzo del programa.
+        '''
         lin = self.sp.readline()
         while not lin.startswith('pT '):
             lin = self.sp.readline()
         ldata = lin[3:].split('/')
         self.timeList.append(ldata[0])
+        nbloque = 0
         for bloque in ldata[1].split('|'):
             self.data.append([])
-            ind = 0
             for word in bloque.split(' '):
-                self.data[ind].append([float(word)])
-                ind += 1
+                self.data[nbloque].append([float(word)])
+            nbloque += 1
         for i in range(1, len(self.data)+1):
             self.ax.append(self.fig.add_subplot(len(self.data)*100+10+i))
-            for _ in range(len(self.data[i])):
-                self.lines.append(self.ax[-1].plot([], [], lw=2))
+            for _ in range(len(self.data[i-1])):
+
+                self.lines.extend(self.ax[-1].plot([], [], lw=2))
+
+    def isRun(self):
+        return self.connected
 
     def updateData(self):
-        while not self.connected:
+        '''Lee el puerto serie hasta encontrar un mensaje del protocolo pyMetria y añade los datos del mensaje valido a la clase'''
+        lin = self.sp.readline()
+        while lin.startswith('pT ') == False:
             lin = self.sp.readline()
-            while lin.startswith('pT ') == False:
-                lin = self.sp.readline()
-            ldata = lin[3:].split('/')
-            self.timeList.append(ldata[0])
-            nbloque = 0
-            for bloque in ldata[1].split('|'):
-                ind = 0
-                for word in bloque.split(' '):
-                    self.data[nbloque][ind].append(float(word))
-                    ind += 1
-                nbloque += 1
+        ldata = lin[3:].split('/')
+        self.timeList.append(ldata[0])
+        nbloque = 0
+        for bloque in ldata[1].split('|'):
+            ind = 0
+            for word in bloque.split(' '):
+                self.data[nbloque][ind].append(float(word))
+                ind += 1
+            nbloque += 1
 
     def debug(self):
+        print "DEBUG -->"
         print self.data
-
-    def run(self):
-        self.connected = True
-        self.thread.start()
+        print self.lines
 
     def stop(self):
         self.connected = False
@@ -80,25 +113,43 @@ class Telemetria:
 
 TELEM = Telemetria('/dev/cu.bqZUM_BT328-SPPDev')
 TELEM.inicializar()
-TELEM.run()
 
 
-# initialization function: plot the background of each frame
+def threadFun():
+    ''' Funcion en un hilo que hace polling sobre el BT para leer datos cada que se escriben'''
+    global TELEM
+    while(TELEM.isRun()):
+        TELEM.updateData()
+
+THREAD = threading.Thread(target=threadFun)
+THREAD.start()
+
+
 def init():
+    '''initialization function: plot the background of each frame'''
     global TELEM
     for line in TELEM.getLines():
         line.set_data([], [])
     return TELEM.getLines()
 
 def updatePlot(i):
+    ''' actualización de las gráficas.
+    Se ajustan automaticamente los ejes X e Y.
+    '''
     global TELEM
     ind = 0
     for i in range(len(TELEM.getData())):
-        ltotal = [item for sublist in TELEM.getData()[i] for item in sublist]
-        TELEM.ax[i].set_ylim(min(ltotal), max(ltotal))
-        TELEM.ax[i].set_xlim(min(TELEM.getTimeList()), max(TELEM.getTimeList()))
+        numpoints = min([len(TELEM.getTimeList()), 100])
+        ltotal = [item for sublist in TELEM.getData()[i] for item in sublist[-numpoints:]]
+        mmin, mmax = min(ltotal)*1.1, max(ltotal)*0.9
+        ax = TELEM.getAx(i)
+        ax.set_ylim([mmin, mmax])
+        lasttime = 0
+        if len(TELEM.getTimeList()) > 0:
+            lasttime = TELEM.getTimeList()[-1]
+        ax.set_xlim([int(lasttime)-ANCHO_X_SEG, int(lasttime)])
         for j in range(len(TELEM.getData()[i])):
-            numpoints = min([len(TELEM.getData()[i][j]), 100])
+            numpoints = len( [x for x in TELEM.getData()[i][j] if x > (int(lasttime)-100)])
             lin = TELEM.getLines()
             lin[ind].set_data(TELEM.getTimeList()[-numpoints:], TELEM.getData()[i][j][-numpoints:])
             ind += 1
